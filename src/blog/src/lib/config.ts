2d97583c3dd7dccf27815dcd4f1240625ead1037
ysasaki <sskyh0208@gmail.com>
2024-11-07 10:00:42 +0900

    色々しゅうせい

---
+++ src/blog/src/lib/config.ts
@@ -0,0 +1,35 @@
+// ブログのタイトル
+export const blogTitle = "ちらしの裏";
+
+// メニューの項目
+export const menuItems = [
+  { name: "home", href: "/" },
+  { name: "about", href: "/about" },
+  { name: "posts", href: "/posts/1" },
+  { name: "tags", href: "/tags" }
+];
+
+// フッターのメニューの項目
+export const footerMenuItems = [
+  { name: "about", href: "/about" },
+  { name: "terms", href: "/terms" },
+  { name: "privacy", href: "/privacy" },
+  { name: "contact", href: "/contact" }
+];
+
+// 1ページあたりの記事数
+export const postsPerPage = 6;
+
+export const author = {
+  icon: "/author.jpg",
+  name: "ささきよ",
+  place: "広島市",
+  occupation: "インフラエンジニア",
+  hobbies: ["プログラミング", "読書", "音楽"],
+  description: "色々やってきて、なんだかんだでITの世界の末席に加わってます。",
+
+  socailLinks: [
+    { name: "github", url: "https://github.com/sskyh0208" },
+    { name: "x", url: "https://x.com/sskyh0208" },
+  ],
+};
